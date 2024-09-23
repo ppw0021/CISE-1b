c3ef7f9de514134ebb4581c96ddea869906c0028
ppw0021 <ppw0021@autuni.ac.nz>
2024-09-23 22:38:20 +1200

    Primitive Navigation
    Simple Linked Button between home and main page.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from 'next/link';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <header>
+        <p>This is a custom homepage built using Next.js and TypeScript.</p>
+      <Link href="/">
+        <button>Main Page</button>
+      </Link>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ frontend/src/app/main/page.tsx
@@ -0,0 +1,9 @@
+export default function MainPage() {
+    return (
+      <div>
+        <h1>Logged In</h1>
+        <p>This is the main page content.</p>
+      </div>
+    );
+  }
+  
