d0cfb4b545597ca6fdd52b561eaa02ec1095eb4f
Declan <ppw0021@autuni.ac.nz>
2024-09-23 16:29:14 +1200

    Reinstall Backend

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -2,14 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const dotenv = require("dotenv");
 async function bootstrap() {
-    dotenv.config();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const port = process.env.PORT || 7000;
-    await app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
