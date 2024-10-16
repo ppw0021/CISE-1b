e429c7999ef083f7043561de52cc4c45651322a6
ppw0021 <ppw0021@autuni.ac.nz>
2024-10-16 17:51:56 +1300

    Fixed for CI/CD

--- frontend/src/app/(notifications)/notifications/page.tsx
+++ frontend/src/app/(notifications)/notifications/page.tsx
@@ -8,22 +8,9 @@ interface Notification {
 }
 
 export default function Home() {
-  const [storedAuthToken, setStoredAuthToken] = useState<string | null>(null);
-  const [isUserAdmin, setAdminStatus] = useState<string | null>(null);
-  const [isUserMod, setModStatus] = useState<string | null>(null);
-  const [isUserAnalyst, setAnalystStatus] = useState<string | null>(null);
   const [notifications, setNotifications] = useState<Notification[]>([]);
 
   useEffect(() => {
-    //Retrieve auth token from localStorage when the component mounts
-    const token = localStorage.getItem('auth_token');
-    const isAdmin = localStorage.getItem('is_admin');
-    const isMod = localStorage.getItem('is_mod');
-    const isAnalyst = localStorage.getItem('is_analyst');
-    setAdminStatus(isAdmin);
-    setModStatus(isMod);
-    setAnalystStatus(isAnalyst);
-    setStoredAuthToken(token);
     getNotifications();
   }, []);
 
